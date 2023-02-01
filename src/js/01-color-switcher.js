0da5ef1efed824eb5b0a6d62b8708c6cc95280c2
Butcherok <butcherok231@gmail.com>
2023-02-01 09:50:29 +0200

    task01 -done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const objBody = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let intervalChangeColor = null;
+
+startBtn.addEventListener('click', onChangeColor);
+stopBtn.addEventListener('click', onStopedChanges);
+
+function onChangeColor () {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalChangeColor = setInterval(() => {
+        objBody.style.backgroundColor = getRandomHexColor();
+    }, 1000
+    )
+};
+
+function onStopedChanges () {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(intervalChangeColor);
+};
