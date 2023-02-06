28807a94a7e3a476067dfcb92cb736861e153a9d
Butcherok <butcherok231@gmail.com>
2023-02-06 11:22:45 +0200

    task03 - not good

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+console.log(form);
+
+form.addEventListener('input', onInputData);
+form.addEventListener('submit', onSubmitForm);
+
+let inputData = {};
+let position = 0;
+let delay = null;
+
+function onInputData(evt) {
+  inputData[evt.target.name] = evt.target.value;
+}
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  amountTimes();
+  form.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(isFulfill(position, delay));
+      } else {
+        reject(isReject(position, delay));
+      }
+    }, delay);
+  }); 
+}
+
+function isFulfill(position, delay) {
+  Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function isReject(position, delay) {
+  Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+}
+
+
+
+function amountTimes() {
+  const amount = inputData.amount;
+  delay = Number(inputData.delay);
+    for(var i = 0; i < amount; i += 1){
+      position += 1;
+      delay = Number(inputData.delay) + Number(inputData.step)*(position - 1);
+      createPromise(position, delay);
+    };
 }
